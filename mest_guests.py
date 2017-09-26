3207bd6794543483ac58461ca68a7d7dbd707c15
chesskibet <flavian.chessesio@meltwater.org>
2017-09-26 10:11:43 +0000

    PEP-8 compliance update

--- mest_guests.py
+++ mest_guests.py
@@ -17,34 +17,34 @@ from smtplib import SMTP
 guests={} # Dictionary where guests are added to
 
 def add_guests(name,email): #Function to create new key value pair in the guest dictionary
-	guests[name] = email
+    guests[name] = email
 
 def list_guests(): # funtion to print a list of all guests attending 
-	for guest,email in guests.items():
-		print(guest,email)
+    for guest,email in guests.items():
+        print(guest,email)
 
 def say_thankyou(): #funtion to send thankyou mail
-	for guest_name, guest_email in guests.items(): # loop through the dictionary and unpack
+    for guest_name, guest_email in guests.items(): # loop through the dictionary and unpack
 
-		# smtp set-up
-		host = "smtp.gmail.com"
-		port = 587
-		username = "chessmestclass@gmail.com"
-		password = "weakpassword"
+        # smtp set-up
+        host = "smtp.gmail.com"
+        port = 587
+        username = "chessmestclass@gmail.com"
+        password = "weakpassword"
 
-		email_connection = SMTP(host, port)
-		email_connection.ehlo()
-		email_connection.starttls()
-		email_connection.login(username,password)
-		
-		if len(guest_name) % 2 == 0: # condtion to answer part D of the question.
+        email_connection = SMTP(host, port)
+        email_connection.ehlo()
+        email_connection.starttls()
+        email_connection.login(username,password)
 
-			email_connection.sendmail(username, guest_email,"Subject: We Appreciate You!\n\nThank you {} for showing up. Come again next time".format(guest_name))
+        if len(guest_name) % 2 == 0: # condtion to answer part D of the question.
 
-		else:
-			email_connection.sendmail(username, guest_email,"Subject: We Appreciate You!\n\nThank you {} for showing up but you are unwelcome at future events!".format(guest_name))
+            email_connection.sendmail(username, guest_email,"Subject: We Appreciate You!\n\nThank you {} for showing up. Come again next time".format(guest_name))
 
-		email_connection.quit() # sign out after sending email
+        else:
+            email_connection.sendmail(username, guest_email,"Subject: We Appreciate You!\n\nThank you {} for showing up but you are unwelcome at future events!".format(guest_name))
+
+        email_connection.quit() # sign out after sending email
 
 #create a while loop to take user input and run required function
 
@@ -52,22 +52,22 @@ a = 0
 
 while a != 3:
 
-	a = 0
-	choice = input("Make a choice:\n1 to add guest\n2 to list guestss\n3 to send thank you email\n4 to exit\n")
-	
-	if choice == "1":
-		name = input("Enter guest's name:\n")
-		email = input("Enter guest's email:\n")
-		add_guests(name,email)
+    a = 0
+    choice = input("Make a choice:\n1 to add guest\n2 to list guestss\n3 to send thank you email\n4 to exit\n")
 
-	elif choice == "2":
-		list_guests()
+    if choice == "1":
+        name = input("Enter guest's name:\n")
+        email = input("Enter guest's email:\n")
+        add_guests(name,email)
 
-	elif choice == "3":
-		say_thankyou()
+    elif choice == "2":
+        list_guests()
 
-	elif choice == "4":
-		a = 3
+    elif choice == "3":
+        say_thankyou()
 
-	else:
-		print("Choose wisely! Invalid choice!!!")
+    elif choice == "4":
+        a = 3
+
+    else:
+        print("Choose wisely! Invalid choice!!!")
